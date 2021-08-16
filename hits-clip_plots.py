5ee2d38b444c6b84b311f451b42477b1a12d7c35
Justin Aruda <justinmakena@gmail.com>
2021-08-16 18:33:01 -0400

    Updated hits-clip_plots.py

--- hits-clip_plots.py
+++ hits-clip_plots.py
@@ -13,6 +13,7 @@ import re
 import matplotlib.cm as cm
 
 table_df = pd.read_table(r'C:/Users/justi/Desktop/PKR HITS-CLIP/CTK/differential_analysis_gencode/snakemake/new/aggregated_sample_summary.txt')
+#table_df = pd.read_table(r'C:/Users/justi/Desktop/PKR HITS-CLIP/CTK/CITS/snakemake_gencode/aggregated_sample_summary.txt')
 
 name_regex = re.compile('(.*)_[^_]*$')
 
@@ -31,7 +32,16 @@ utrs = table_df[table_df['Annotation'].str.contains('_UTR')]
 introns = table_df[table_df['Annotation'].str.contains('_transcript')]
 sno_rnas = table_df[table_df['Annotation'].str.contains('SNO')]
 
-# def stacked_bar(libraries,groups):
+def stacked_bar(input_file,libraries,groups):
+    input_df = pd.read_table(input_file)
+    for library in libraries:
+        genes = input_df[input_df['Annotation'].str.contains('_gene')]
+        exons = input_df[input_df['Annotation'].str.contains('_exon')]
+        utrs = input_df[input_df['Annotation'].str.contains('_UTR')]
+        introns = input_df[input_df['Annotation'].str.contains('_transcript')]
+        
+        
+        
     
 
 add_groups = ['SNHG11', 'SNHG12', 'SNHG15', 'SNHG16', 'SNHG17', 'SNHG19', 'SNHG25', 'SNHG26', 'SNHG29', 'SNHG3', 'SNHG30', 'SNHG4', 'SNHG5', 'SNHG6', 'SNHG7', 'SNHG8', 'SNHG1', 'GAS5', 'RPS12', 'RPS2', 'RPS8', 'RPSA', 'RPL17', 'RPL23A', 'RPL4', 'RPL7A', 'GNL3', 'RCC1', 'RPL13A', 'RPL27A']
@@ -65,7 +75,6 @@ def scatter_plot(group1,group2,key,groups):
             leftover_cond = leftover_df['Annotation'].str.contains(series_key)
         
         series_df = table_df[cond]
-        print(series_df)
         leftover_df = leftover_df[~leftover_cond]
         x = series_df[group1]
         y = series_df[group2]
@@ -75,6 +84,7 @@ def scatter_plot(group1,group2,key,groups):
     mplcursors.cursor(scatters['SNORD'][0],multiple=True).connect("add", lambda sel: sel.annotation.set_text(scatters['SNORD'][1][sel.target.index]))
     mplcursors.cursor(scatters['SNORA'][0],multiple=True).connect("add", lambda sel: sel.annotation.set_text(scatters['SNORA'][1][sel.target.index]))
     mplcursors.cursor(scatters['MT-'][0],multiple=True).connect("add", lambda sel: sel.annotation.set_text(scatters['MT-'][1][sel.target.index]))
+    mplcursors.cursor(scatters['Embedded snoRNA'][0],multiple=True).connect("add", lambda sel: sel.annotation.set_text(scatters['Embedded snoRNA'][1][sel.target.index]))
     
     x = leftover_df[group1]
     y = leftover_df[group2]
@@ -108,4 +118,6 @@ def scatter_plot(group1,group2,key,groups):
     
     plt.title('{0} vs. {1}'.format(group1,group2))
 
-scatter_plot('A1KO_Mock_Down_Normalized_Tags_Assigned','A1KO_IFN_Down_Normalized_Tags_Assigned','_gene',[("Embedded snoRNA",add_groups),'SNORD','SNORA','MT-'])
+scatter_plot('A1KO_Mock_Down_Normalized_Tags_Total','A1KO_IFN_Down_Normalized_Tags_Total','_gene',[("Embedded snoRNA",add_groups),'SNORD','SNORA','MT-'])
+#scatter_plot('A1KO_Mock_Down_Normalized_Tags_Assigned','A1KO_IFN_Down_Normalized_Tags_Assigned','_gene',[("Embedded snoRNA",add_groups),'SNORD','SNORA','MT-'])
+#scatter_plot('A1KO_Mock_Down_Normalized_Tags_Assessed','A1KO_IFN_Down_Normalized_Tags_Assessed','_gene',[("Embedded snoRNA",add_groups),'SNORD','SNORA','MT-'])
